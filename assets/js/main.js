a9bee6f04a64ea6a69d35e990ad779ba8391a486
Yumi Kim <yumikimsk@gmail.com>
2016-04-17 03:30:51 -0400

    add footer links and style a tags

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,12 +6,23 @@ $(document).ready(function() {
 
 function loadFooter() {
     var copyrightContainer = $('#footer-copyright');
+    var newsContainer = $('#footer-news');
+    var linksContainer = $('#footer-links');
+    var socialContainer = $('#footer-social');
 
-    var jqxhr = $.getJSON(urlAbout)
+    var jqxhr = $.getJSON(urlFooter)
     .done(function(data) {
         var copyright = data.copyright.html;
+        var news = data.news;
+        var links = data.quickLinks;
 
         copyrightContainer.html(copyright);
+        newsContainer.html("<h1>NEWS</h1><p><a href='" + news + "'>View News</a></p>");
+        linksContainer.html("<h1>QUICK LINKS</h1>");
+        $.each(links, function(i, link) {
+            linksContainer.append("<a href='http://" + link.href + "'>" + link.title + "</a><br>");
+        });
+        socialContainer.html("<h1>SOCIAL MEDIA</h1>");
     })
     .fail(function() {
         console.log("error loading json stream from " + urlFooter);
