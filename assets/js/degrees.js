303f395a5dff74c0a2f3c78a60a25b255b604d59
Yumi Kim <yumikimsk@gmail.com>
2016-04-17 19:48:35 -0400

    add content and styling to degrees page

--- assets/js/degrees.js
+++ assets/js/degrees.js
@@ -9,5 +9,54 @@ $(document).ready(function() {
         $_GET[decode(arguments[1])] = decode(arguments[2]);
     });
 
-    console.log("ready");
+    var urlDegrees = "https://people.rit.edu/~sarics/web_proxy.php?path=degrees";
+    var degree = $_GET['degree'];
+    var level = "";
+    var degreeContainer = $('#degree-content');
+    var html = "";
+
+    var jqxhr = $.getJSON(urlDegrees)
+    .done(function(data) {
+        var undergradDegrees = data.undergraduate;
+        $.each(undergradDegrees, function(i, undergrad) {
+            var name = undergrad.degreeName;
+
+            if (name == degree) {
+                level = "Undergraduate";
+                var title = undergrad.title;
+                var desc = undergrad.description;
+                var concentrations = undergrad.concentrations;
+
+                html = "<h2>" + level + "</h2><h1>" + title + "</h1><p>" + desc + "</p><h3>Concentrations</h3><ul>";
+                $.each(concentrations, function(i, concentration) {
+                    html += "<li>" + concentration + "</li>";
+                })
+                html += "</ul>";
+            }
+        });
+
+        var gradDegrees = data.graduate;
+
+        $.each(gradDegrees, function(i, grad) {
+            var name = grad.degreeName;
+
+            if (name == degree) {
+                level = "Graduate";
+                var title = grad.title;
+                var desc = grad.description;
+                var concentrations = grad.concentrations;
+
+                html = "<h2>" + level + "</h2><h1>" + title + "</h1><p>" + desc + "</p><h3>Concentrations</h3><ul>";
+                $.each(concentrations, function(i, concentration) {
+                    html += "<li>" + concentration + "</li>";
+                })
+                html += "</ul>";
+            }
+        });
+
+        degreeContainer.html(html);
+    })
+    .fail(function() {
+        console.log("error loading json stream from " + urlDegrees);
+    });
 });
