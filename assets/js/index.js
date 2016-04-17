4719d883fa5ed04f27552b0aaba2529b6618aa1e
Yumi Kim <yumikimsk@gmail.com>
2016-04-17 17:34:16 -0400

    add news

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,12 @@
 var urlAbout = "https://people.rit.edu/~sarics/web_proxy.php?path=about";
+var urlNews = "https://people.rit.edu/~sarics/web_proxy.php?path=news";
 var urlFooter = "https://people.rit.edu/~sarics/web_proxy.php?path=footer";
 
 $(document).ready(function() {
     loadAbout();
+    loadDegrees();
+    loadMinors();
+    loadNews();
     loadSocial();
 });
 
@@ -18,13 +22,45 @@ function loadAbout() {
         var author = data.quoteAuthor;
 
         aboutContainter.html("<h1>Information Sciences and Technology at RIT</h1><h2>" + title + "</h2><p>" + desc + "</p>");
-        quoteContainer.html("<p id='quote'><i id='quote-left' class='fa fa-quote-left fa-2x'></i>" + quote + "<i id='quote-right' class='fa fa-quote-right fa-2x'></i></p><p>" + author + "</p>");
+        quoteContainer.html("<h1>TESTIMONY</h1><p id='quote'><i id='quote-left' class='fa fa-quote-left fa-2x'></i>" + quote + "<i id='quote-right' class='fa fa-quote-right fa-2x'></i></p><p>" + author + "</p>");
     })
     .fail(function() {
         console.log("error loading json stream from " + urlAbout);
     });
     jqxhr.complete(function() {
 
+    });
+}
+
+function loadDegrees() {
+
+}
+
+function loadMinors() {
+
+}
+
+function loadNews() {
+    var newsContainter = $('#index-news-content');
+
+    var jqxhr = $.getJSON(urlNews)
+    .done(function(data) {
+        var year = data.year;
+        var html = "<h1>NEWS</h1><div class='slider'>";
+
+        $.each(year, function(i, news) {
+            var date = news.date;
+            var title = news.title;
+            var desc = news.description.substring(0,244) + "...";
+
+            html += "<a data-remodal-target='modal' href='#'><div class='news-item'><h2 class='news-title'>" + title + "</h2><p class='news-date'>" + date + "</p><p class='news-desc'>" + desc + "</p></div></a>";
+        });
+
+        html += "</div>";
+        newsContainter.html(html);
+    })
+    .fail(function() {
+        console.log("error loading json stream from " + urlNews);
     });
 }
 
